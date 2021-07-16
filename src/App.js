36c8806b9fee455225ad600659ded365296f014f
gerardodev25 <gerardodev25@gmail.com>
2021-07-16 08:33:22 -0400

    add ant design libreria de componentes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.scss";
 
+import { DatePicker } from "antd";
+
+function onChange(date, dateString) {
+   console.log(date, dateString);
+}
+
 function App() {
    return (
       <div className="App">
          <h1>React Client</h1>
          <h2>Proyercto</h2>
+         <DatePicker onChange={onChange} picker="week" />
       </div>
    );
 }
