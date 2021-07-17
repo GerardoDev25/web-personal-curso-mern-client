fb213d909b9fb77744718aefe71be1951a9dd4ba
gerardodev25 <gerardodev25@gmail.com>
2021-07-17 09:37:15 -0400

    install react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,19 +1,61 @@
+import {
+   BrowserRouter as Router,
+   Switch,
+   Route,
+   Link,
+   Redirect,
+} from "react-router-dom";
 import "./App.scss";
-
-import { DatePicker } from "antd";
-
-function onChange(date, dateString) {
-   console.log({ date, dateString });
-}
 
 function App() {
    return (
-      <div className="App">
-         <h1>React Client</h1>
-         <h2>Proyercto</h2>
-         <DatePicker onChange={onChange} picker="week" />
-      </div>
+      <Router>
+         <div className="App">
+            <h1>Sistemas de rutas basicas</h1>
+
+            <nav>
+               <ul>
+                  <li>
+                     <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                     <Link to="/contact">Contact</Link>
+                  </li>
+                  <li>
+                     <Link to="/users">Users</Link>
+                  </li>
+               </ul>
+            </nav>
+
+            <Switch>
+               <Route exact path="/" component={Home} />
+               <Route
+                  exact
+                  path="/contact"
+                  component={Contact}
+               />
+               <Route exact path="/users" component={Users} />
+
+               <Route component={Error404} />
+            </Switch>
+         </div>
+      </Router>
    );
 }
 
+function Home() {
+   return <h2>Estamos en el componenete home</h2>;
+}
+
+function Contact() {
+   return <h2>Componenete contact</h2>;
+}
+
+function Users() {
+   return <h2>Estamos en el componenete user</h2>;
+}
+
+function Error404() {
+   return <h2>Error 404</h2>;
+}
 export default App;
